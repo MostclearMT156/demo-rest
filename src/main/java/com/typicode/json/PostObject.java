4fcec2485063b8fb41227a59a5b84043bc7b33e2
Administrator <djycnsuxfl@gmail.com>
2026-03-15 03:14:12 +0800

    add constructor

--- src/main/java/com/typicode/json/PostObject.java
+++ src/main/java/com/typicode/json/PostObject.java
@@ -1,8 +1,32 @@
 package com.typicode.json;
 
 public class PostObject {
-    int userId;
-    int id;
-    String title;
-    String body;
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+
+    public PostObject(int userId, int id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
 }
