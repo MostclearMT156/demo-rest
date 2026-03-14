7efcaf60bc697dfbd7854871d40b238fea32c1da
Administrator <djycnsuxfl@gmail.com>
2026-03-15 05:13:11 +0800

    add testCreatePost

--- src/test/java/com/typicode/json/RestTest.java
+++ src/test/java/com/typicode/json/RestTest.java
@@ -9,4 +9,13 @@ public class RestTest {
     public void testGetPostById() {
         given().when().get("https://jsonplaceholder.typicode.com/posts/1").then().log().body();
     }
+    @Test
+    public void createPost() {
+        String body = "{\n" +
+                "    \"title\": \"Мое название\",\n" +
+                "    \"body\": \"Это моя первая новость\",\n" +
+                "    \"userId\": 1000\n" +
+                "}";
+        given().header("Content-type", "application/json; charset=UTF-8").body(body).when().post("https://jsonplaceholder.typicode.com/posts").then().log().body();
+    }
 }
